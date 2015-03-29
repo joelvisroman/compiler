4ac88047df3861c528dc71d1f0eb978c2769bbd5
Joelvis Roman <joelvis.roman@gmail.com>
2015-03-29 00:12:03 -0300

    Adicionado TMemo

--- compilador.pas
+++ compilador.pas
@@ -5,7 +5,7 @@ interface
 uses
   Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
   Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.ExtCtrls, Vcl.StdCtrls, Vcl.Buttons,
-  AdvMemo;
+  AdvMemo, AdvSplitter, Vcl.Menus, Vcl.ComCtrls, propscrl;
 
 type
   Tfrmcompilador = class(TForm)
@@ -20,6 +20,9 @@ type
     btgerarcodigo: TSpeedButton;
     btequipe: TSpeedButton;
     AdvMemo1: TAdvMemo;
+    Splitter1: TSplitter;
+    StatusBar1: TStatusBar;
+    AdvMemo2: TAdvMemo;
   private
     { Private declarations }
   public
